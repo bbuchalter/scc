26b9c2a01c427cf83210a7774f05d53601030b68
Brian Buchalter <bal711@gmail.com>
2018-02-06 08:22:27 -0700

    Test coverage for rounding call counts

--- test/timer_test.rb
+++ test/timer_test.rb
@@ -6,4 +6,14 @@ class TimerTest < Minitest::Test
     SCC::Timer.new(interval: 3, duration: 6).call { counter += 1 }
     assert_equal(2, counter)
   end
+
+  def test_timer_invokes_block_always_rounds_down_call_counts
+    counter = 0
+    SCC::Timer.new(interval: 3, duration: 7).call { counter += 1 }
+    assert_equal(2, counter)
+
+    counter = 0
+    SCC::Timer.new(interval: 3, duration: 8).call { counter += 1 }
+    assert_equal(2, counter)
+  end
 end
