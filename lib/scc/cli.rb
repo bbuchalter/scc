ec3d236292af064a9c0f5eb8e41e945cbd48df23
Brian Buchalter <bal711@gmail.com>
2018-02-06 12:34:47 -0700

    Update CLI to use CollectResponseTimes

--- lib/scc/cli.rb
+++ lib/scc/cli.rb
@@ -4,7 +4,12 @@ require 'scc'
 class CLI < Thor
   desc 'average_response_time URL', 'Send an HTTP request to URL every 10 seconds for 60 seconds and calculate the average response time of the responses.'
   def average_response_time(url)
-    # responses = SCC::CollectResponses.new(url: url).call
+    response_times = SCC::CollectResponseTimes.new(
+      url: url,
+      interval: 10,
+      duration: 60
+    ).call
+    puts response_times.inspect
     # puts SCC::Reporting::Text.new(respones).call
   end
 end
