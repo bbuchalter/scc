34eaf354b2373dcdc1bc273250bedbb69dd36781
Brian Buchalter <bal711@gmail.com>
2018-02-06 08:05:06 -0700

    Define SCC::VERSION after SCC is defined
    The reason `scc.rb` exists is to define the top level module namespace.
    By requiring `version.rb` before that namespace exists, we create that
    namespace implicilty.

--- lib/scc.rb
+++ lib/scc.rb
@@ -1,8 +1,7 @@
-require "scc/version"
-
 # Top level namespace for SiteCheckCLI (SCC)
 module SCC
 end
 
 # After top level namespace is defined, we can start requiring into that namespace.
+require "scc/version"
 require 'scc/collect_responses'
