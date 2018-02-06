3b46d436a2fd8ac1f8abf6b0885e771975cb9c79
Brian Buchalter <bal711@gmail.com>
2018-02-06 11:41:44 -0700

    Implement TimeHttpResponse
    Introduced a bit of mocking and stubbing to ensure there are no actual
    network calls in the test suite. Required injecting the HTTP get call,
    which is reasonable.

--- lib/scc/time_http_response.rb
+++ lib/scc/time_http_response.rb
@@ -1,13 +1,19 @@
+require 'benchmark'
+
 module SCC
   # Given a URL, return a Float representing the time get a response for an
   # HTTP request to that URL.
   class TimeHttpResponse
-    def initialize(url:)
+    def initialize(url:, http_get:)
       @url = url
+      @http_get = http_get
     end
 
     def call
-      0.0
+      time = Benchmark.measure do
+        @http_get.call
+      end
+      time.real
     end
   end
 end
--- test/time_http_response_test.rb
+++ test/time_http_response_test.rb
@@ -1,10 +1,35 @@
 require "test_helper"
+require "minitest/stub/const"
 
 class TimeHttpResponseTest < Minitest::Test
-  def test_call_returns_a_float
-    assert_equal(
-      Float,
-      SCC::TimeHttpResponse.new(url: 'https://google.com').call.class
-    )
+  def test_benchmark_and_http_are_used_to_time_responses
+    # Create a mock HTTP get that has a call method with no implementation
+    http_get_mock = Minitest::Mock.new
+    def http_get_mock.call; end
+
+    # Create a mock BenchmarkTms object to be returned from Benchmark#measure
+    benchmark_timing_mock = Minitest::Mock.new
+    benchmark_timing_mock.expect(:real, 1.23)
+
+    # Create a mock Benchmark to return a mock BenchmarkTms objet
+    benchmark_mock = Minitest::Mock.new
+    benchmark_mock.expect(:measure, benchmark_timing_mock)
+
+    # Stub Benchmark with our mock
+    Object.stub_const :Benchmark, benchmark_mock do
+
+      # Assert we get the expected value back from Benchmark#measure
+      assert_equal(
+        1.23,
+        SCC::TimeHttpResponse.new(
+          url: 'https://google.com',
+          http_get: http_get_mock
+        ).call
+      )
+    end
+
+    # Assert our mock expectations were satisifed
+    assert_mock benchmark_timing_mock
+    assert_mock benchmark_mock
   end
 end
