d97f2200843ea861b796e93bb7ad453b525bca6e
Brian Buchalter <bal711@gmail.com>
2018-02-06 08:44:45 -0700

    Trim TimerTest for speed
    Although we haven't optimized the execution, we can modify our a test
    cases just a bit to cut down on test run time.

--- test/timer_test.rb
+++ test/timer_test.rb
@@ -3,18 +3,14 @@ require "test_helper"
 class TimerTest < Minitest::Test
   def test_timer_invokes_block_the_correct_number_of_times
     counter = 0
-    SCC::Timer.new(interval: 3, duration: 6).call { counter += 1 }
+    SCC::Timer.new(interval: 1, duration: 2).call { counter += 1 }
     assert_equal(2, counter)
   end
 
   def test_timer_always_rounds_down_call_counts
     counter = 0
-    SCC::Timer.new(interval: 3, duration: 7).call { counter += 1 }
-    assert_equal(2, counter)
-
-    counter = 0
-    SCC::Timer.new(interval: 3, duration: 8).call { counter += 1 }
-    assert_equal(2, counter)
+    SCC::Timer.new(interval: 2, duration: 3).call { counter += 1 }
+    assert_equal(1, counter)
   end
 
   def test_timer_honors_interval
